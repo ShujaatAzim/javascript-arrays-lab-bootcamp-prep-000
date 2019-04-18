7166f21fe73c7d52cc1a7357279a0a515f71a0d1
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-18 21:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
+
+function appendKitten(name) {
+  var newArr = [...kittens];
+  newArr.push(name);
+  return newArr;
+}
